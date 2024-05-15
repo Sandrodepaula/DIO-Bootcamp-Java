1dc6b2be95ea9c2438ab95ca2ef1a7477695a430
Sandro <sandroads@yahoo.com>
2024-05-14 22:43:49 -0300

    anatomia das classes

---
+++ java-anatomia-classes/src/edu/sandro/anatomiadasclasses/MinhaClasse.java
@@ -0,0 +1,17 @@
+package edu.sandro.anatomiadasclasses;
+public class MinhaClasse{
+
+    public static void main (String [] args) {
+        String primeiroNome = "Sandro";
+        String segundoNome = "Santos";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);  
+    }
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+    
+}
