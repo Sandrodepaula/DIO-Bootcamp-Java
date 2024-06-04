1c3b3bb099fc1023011710b45a3e697a19402b14
Sandro <sandroads@yahoo.com>
2024-06-04 20:44:30 -0300

    Sistema para controle de candidatos

---
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -0,0 +1,22 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args) throws Exception {
+    analisarCandidato(1900.0);
+    analisarCandidato(2200.0);
+    analisarCandidato(2000.0);
+    
+    }
+    
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+
+        }else if(salarioBase == salarioPretendido){
+            System.out.println("LIGAR COM UMA CONTRA PROPOSTA");
+        }else{
+            System.out.println("AGUARDAR OS OUTROS CANDIDATOS");
+        }
+
+    }
+}
