39d8c41b75f0008622b6e027f700821d5af73a36
Sandro <sandroads@yahoo.com>
2024-06-13 23:11:34 -0300

    Verificador de número

--- DesafioContaBancaria/src/SimulacaoBancaria.java
+++ DesafioContaBancaria/src/SimulacaoBancaria.java
@@ -14,7 +14,7 @@ public class SimulacaoBancaria {
 
             switch (opcao) {
                 case 1:
-                // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
+                //Ler o valor a ser depositado e atualizar/imprimir o saldo.
                     System.out.println("Digite o valor a ser depositado: ");
                     double deposito = scanner.nextDouble();
                     saldo += deposito;
@@ -22,20 +22,21 @@ public class SimulacaoBancaria {
                     
                     break;
                 case 2:
-                    // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    /*Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.*/
                     System.out.println("Digite o valor a ser sacado: ");
                     double saque = scanner.nextDouble();
-                    saldo -= saque;
+                    
                     if (saque > saldo){
                        System.out.println(" Saldo insuficiente ");
 
                     } else{
+                        saldo -= saque;
                         System.out.println("Saldo atual: " + saldo);
                     }
                       
                     break;
                 case 3:
-                    // TODO: Exibir o saldo atual da conta.
+                    //Exibir o saldo atual da conta.
                     System.out.println("Saldo atual: " + saldo );
                     break;
                 case 0:
