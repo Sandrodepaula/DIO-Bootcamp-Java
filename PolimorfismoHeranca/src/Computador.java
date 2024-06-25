e50a708df9dd8edfec8a5cbc715d2792fdbb8e29
Sandro <sandroads@yahoo.com>
2024-06-24 23:04:24 -0300

    Programação orientada a objeto

---
+++ PolimorfismoHeranca/src/Computador.java
@@ -0,0 +1,16 @@
+public class Computador {
+    public static void main(String[] args) throws Exception {
+        MSNMessenger msn = new MSNMessenger();
+        msn.enviarMensagem();
+        msn.receberMensagem();
+
+        FacebookMessenger fcb = new FacebookMessenger();
+        fcb.enviarMensagem();
+        fcb.receberMensagem();
+
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
+        
+    }
+}
