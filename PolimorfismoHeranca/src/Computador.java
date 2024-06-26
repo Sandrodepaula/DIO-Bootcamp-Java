62ef6166171fc7c0518e897252164bed6cabc465
Sandro <sandroads@yahoo.com>
2024-06-25 21:52:48 -0300

    Polimorfismo

--- PolimorfismoHeranca/src/Computador.java
+++ PolimorfismoHeranca/src/Computador.java
@@ -1,17 +1,23 @@
+import apps.FacebookMessenger;
+import apps.MSNMessenger;
+import apps.ServicoMensagemInstantanea;
+import apps.Telegram;
+
 public class Computador {
     public static void main(String[] args) throws Exception {
+        ServicoMensagemInstantanea smi = null;
+
+        String appEscolhido = "fbm";
+
+        if (appEscolhido.equals ("msn"))
+            smi = new MSNMessenger();
+        else if(appEscolhido.equals("fbm"))
+            smi = new FacebookMessenger();
+        else if(appEscolhido.equals("tlg"))
+            smi = new Telegram();
         
-        MSNMessenger msn = new MSNMessenger();
-        msn.enviarMensagem();
-        msn.receberMensagem();
-
-        FacebookMessenger fcb = new FacebookMessenger();
-        fcb.enviarMensagem();
-        fcb.receberMensagem();
-
-        Telegram tlg = new Telegram();
-        tlg.enviarMensagem();
-        tlg.receberMensagem();
+        smi.enviarMensagem();
+        smi.receberMensagem();
         
     }
 }
