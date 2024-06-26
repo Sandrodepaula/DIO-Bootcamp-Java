64cb113ea5d106acad227e241c30c0098dbdd10e
Sandro <sandroads@yahoo.com>
2024-06-25 22:42:10 -0300

    interfaces

---
+++ interfaces/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
@@ -0,0 +1,24 @@
+package equipamentos.multifuncional;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
+public class EquipamentoMultifuncional implements Copiadora, Digitalizadora, Impressora {
+
+    public void copiar() {
+        System.out.println("Copiando via equipamento multifuncional");
+    }
+
+   
+    public void digitalizar() {
+        System.out.println("digitalizando via equipamento multifuncional");
+    }
+
+   
+    public void imprimir() {
+        System.out.println("Imprimindo via equipamento multifuncional");
+    }
+    
+    
+}
