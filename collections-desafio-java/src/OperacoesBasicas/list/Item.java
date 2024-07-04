6f8c54155d55fae22f2b42399602894985fe7268
Sandro <sandroads@yahoo.com>
2024-07-04 17:43:57 -0300

    Collections - List

---
+++ collections-desafio-java/src/OperacoesBasicas/list/Item.java
@@ -0,0 +1,33 @@
+package OperacoesBasicas.list;
+
+public class Item {
+    private String nome;
+    private double preco;
+    private int quant;
+
+    public Item (String nome, double preco, int quant){
+        this.nome = nome;
+        this.preco = preco;
+        this.quant = quant;
+    }
+
+    public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public int getQuant() {
+		return quant;
+    }
+    public String toString() {
+            return "Item{" +
+                    "nome='" + nome + '\'' +
+                    ", preco=" + preco +
+                    ", quant=" + quant +
+                    '}';
+    }
+    
+}
